a411e59671a700286865fc0b53a33eaeb246aea8
vigilanteX <dixitaman8@gmail.com>
2025-08-08 20:49:14 +0530

    Data provider

---
+++ src/test/java/org/example/P6DataProvider.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class P6DataProvider {
+    @DataProvider
+    public Object[][] dataprovider_method() {
+
+        return new Object[][]{{"aman","dixit"},{"xyz","dixit"}};
+    }
+
+    @Test(dataProvider = "dataprovider_method")
+    public void m1(String val,String val2) {
+
+        System.out.println(val+" "+val2);
+
+    }
+
+
+}
